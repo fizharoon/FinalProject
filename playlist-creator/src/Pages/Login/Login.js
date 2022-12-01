9c5cb198fd0292cfd42c000cad0b889a06043de8
Fiza Haroon <fizharoon@gmail.com>
2022-12-01 10:48:17 -0800

    Updated Login.js with styling and html, working on register

--- playlist-creator/src/Pages/Login/Login.js
+++ playlist-creator/src/Pages/Login/Login.js
@@ -6,9 +6,30 @@ const Login = () => {
   useEffect(() => {}, []);
 
   return (
-    <main className="dashboard">
-      <h1>Login</h1>
-    </main>
+    <body>
+        <div class="header">
+          <div>
+            <h1>sPooTIfY</h1>
+          </div>
+        </div>
+    
+          <div class="body">
+            <form>
+                <label for="username">Username:</label>
+              <br />
+                <input type="text" id="username" name="username" />
+              <br />
+                <label for="pwd">Password:</label>
+              <br />
+                <input type="password" id="pwd" name="pwd" />
+            </form>{" "}
+            <br />
+            <button>Log In</button>
+            <br />
+            <br />
+            <button>Register</button>
+          </div>
+    </body>
   );
 };
 
