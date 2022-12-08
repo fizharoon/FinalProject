7ce645094aa9c42469b04a64ea28e400dd292530
arizvi7 <arizvi7@ucmerced.edu>
2022-12-08 00:59:27 -0800

    Modified these files(please give feedback)

--- playlist-creator/src/Pages/CreatePlaylist/CreatePlaylist.js
+++ playlist-creator/src/Pages/CreatePlaylist/CreatePlaylist.js
@@ -8,6 +8,13 @@ const CreatePlaylist = () => {
   return (
     <main className="dashboard">
       <h1>CreatePlaylist</h1>
+
+      <h2>Select songs</h2>
+        <input type="checkbox" id="song" name="track1" value="track1" />
+        <label for="track1">track1 </label><br />
+        <div id="root"></div>
+        <button id="btn">Submit</button>
+
     </main>
   );
 };
--- playlist-creator/src/Pages/Register/Artist&Genre.js
+++ playlist-creator/src/Pages/Register/Artist&Genre.js
@@ -3,11 +3,27 @@ import "./styles.css";
 
 const PreRegister = () => {
   const [user, setUser] = useState(0);
-  useEffect(() => {}, []);
+  useEffect(() => { }, []);
 
   return (
     <main className="dashboard">
       <h1>PreRegister</h1>
+      <body>
+
+        <h2>Select genres</h2>
+        <input type="checkbox" id="genre" name="acoustic" value="acoustic" />
+        <label for="genre">acoustic </label><br />
+        <div id="root"></div>
+        <button id="btn">Submit</button>
+
+        <h2>Select artists</h2>
+        <input type="checkbox" id="artist" name="Drake" value="Drake" />
+        <label for="Drake">Drake </label><br />
+        <div id="root"></div>
+        <button id="btn">Submit</button>
+
+
+      </body>
     </main>
   );
 };
