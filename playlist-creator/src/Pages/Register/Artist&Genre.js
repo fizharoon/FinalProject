1fffb8493b7c06e0560ffd8d6d0a0e1e0aa616b4
arizvi7 <arizvi7@ucmerced.edu>
2022-12-08 17:09:31 -0800

    test

--- playlist-creator/src/Pages/Register/Artist&Genre.js
+++ playlist-creator/src/Pages/Register/Artist&Genre.js
@@ -9,7 +9,6 @@ const PreRegister = () => {
     <main className="dashboard">
       <h1>PreRegister</h1>
       <body>
-
         <h2>Select genres</h2>
         <input type="checkbox" id="genre" name="acoustic" value="acoustic" />
         <label for="genre">acoustic </label><br />
@@ -21,7 +20,6 @@ const PreRegister = () => {
         <label for="Drake">Drake </label><br />
         <div id="root"></div>
         <button id="btn">Submit</button>
-
       </body>
     </main>
   );
