ef80c7384657d9a32bab03ba0a9eb11d62b060f0
arizvi7 <arizvi7@ucmerced.edu>
2022-12-08 17:16:14 -0800

    another test whereby artist and genre is modified

--- playlist-creator/src/Components/Song.js
+++ playlist-creator/src/Components/Song.js
@@ -9,18 +9,16 @@ const Song = () => {
     <main className="dashboard">
       <h1>Songs</h1>
       <body>
-
         <h2>Select songs</h2>
         <input type="checkbox" id="song" name="song1" value="song1" />
         <label for="song">song </label><br />
         <div id="root"></div>
         <button id="btn">Add </button>
         <button id="btn">Play </button>
-
       </body>
     </main>
   );
-  
+
 };
 
 export default Song;
