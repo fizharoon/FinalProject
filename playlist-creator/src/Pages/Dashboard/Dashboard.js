2a92b665b69d17b531bf9c1f5284ceaa08a61086
Fiza Haroon <fizharoon@gmail.com>
2022-12-08 17:37:33 -0800

    Dashboard page updated , refer to templates folder

--- playlist-creator/src/Pages/Dashboard/Dashboard.js
+++ playlist-creator/src/Pages/Dashboard/Dashboard.js
@@ -1,14 +1,52 @@
 import React, { useEffect, useState } from "react";
 import "./styles.css";
+import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
 
 const Dashboard = () => {
   const [user, setUser] = useState(0);
   useEffect(() => {}, []);
 
   return (
-    <main className="dashboard">
-      <h1>Dashboard</h1>
-    </main>
+/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> */
+
+<body>
+
+<div class="sidebar">
+  <a href="#home"><i class="fa fa-fw fa-home"></i> Home</a>
+  <a href="createplaylist.html"><i class="fa fa-fw fa-plus-square"></i> Create Playlist</a>
+  <a href="playlist.html"><i class="fa fa-fw fa-heart-o"></i> My Songs </a>
+  <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+  <a href="login.html">Logout <i class="fa fa-fw fa-sign-out"></i></a>
+</div>
+
+<div class="main">
+  <h2>My Playlists</h2>
+    <button class="playlist"><img src="album.png"/>Coding Playlist</button>
+  <h2>Recommended Songs</h2>
+    <div >
+        <table id="customer">
+            <thead>
+                <th>Song Name</th>
+                <th>Artist</th>
+                <th>Album</th>
+                <th></th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>How to Code</td>
+                    <td>Ammon</td>
+                    <td>CSE106</td>
+                    <td><button class="add"><i class="fa fa-fw fa-plus"></i></button></td>
+                </tr>
+            </tbody>
+        </table>
+</div>
+
+
+</div>
+
+</body>
+
   );
 };
 
