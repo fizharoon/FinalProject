f1d942653c20fa5f5949a53f7d5cd441182056fa
arizvi7 <arizvi7@ucmerced.edu>
2022-12-10 02:22:20 -0800

    Merged

--- playlist-creator/src/Pages/CreatePlaylist/CreatePlaylist.js
+++ playlist-creator/src/Pages/CreatePlaylist/CreatePlaylist.js
@@ -1,22 +1,67 @@
 import React, { useEffect, useState } from "react";
 import "./styles.css";
-
+import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
 const CreatePlaylist = () => {
   const [user, setUser] = useState(0);
   useEffect(() => {}, []);
 
   return (
-    <main className="dashboard">
-      <h1>CreatePlaylist</h1>
 
-      <h2>Select songs</h2>
-        <input type="checkbox" id="song" name="track1" value="track1" />
-        <label for="track1">track1 </label><br />
-        <div id="root"></div>
-        <button id="btn">Submit</button>
+<body>
 
-    </main>
+    <div class="sidebar">
+        <a href="dashboard.html"><i class="fa fa-fw fa-home"></i> Home</a>
+        <a href="createplaylist.html"><i class="fa fa-fw fa-plus-square"></i> Create Playlist</a>
+        <a href="playlist.html"><i class="fa fa-fw fa-heart-o"></i> My Playlists </a> 
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <a href="login.html">Logout <i class="fa fa-fw fa-sign-out"></i></a>
+
+      </div>
+      <br/>
+    <div class="move">
+        
+        <input type="text" id="playlistname"/>
+        <button>Create</button>
+
+    </div>
+    <br/><br/>
+    <div class="move">
+        <table>
+            <thead>
+                <th>Song Name</th>
+                <th>Artist</th>
+                <th>Album</th>
+            </thead>
+            <tbody>
+                <tr></tr>
+                
+            </tbody>
+        </table>
+    </div>
+    <br/><br/><br/><br/><br/><br/><br/>
+
+    <h2 class="move">Recommended Songs</h2>
+    <div class="move">
+        <table id="customer">
+            <thead>
+                <th>Song Name</th>
+                <th>Artist</th>
+                <th>Album</th>
+                <th></th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>How to Code</td>
+                    <td>Ammon</td>
+                    <td>CSE106</td>
+                    <td><button class="add"><i class="fa fa-fw fa-plus"></i></button></td>
+                </tr>
+            </tbody>
+        </table>
+</div>
+</body>
   );
 };
 
 export default CreatePlaylist;
+
